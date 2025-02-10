75d8c15e27c3661c44f5a65d64d91409d289e011
Oguz Vatansever <ogzvatansever@gmail.com>
2025-02-11 00:44:45 +0300

    day 4

--- 2024/4/main.js
+++ 2024/4/main.js
@@ -2,29 +2,41 @@ if (require.main === module) {
     main();
 }
 
-function main() {
-    var input = [
-                "MMMSXXMASM",
-                "MSAMXMSMSA",
-                "AMXSXMAAMM",
-                "MSAMASMSMX",
-                "XMASAMXAMM",
-                "XXAMMXXAMA",
-                "SMSMSASXSS",
-                "SAXAMASAAA",
-                "MAMMMXMMMM",
-                "MXMXAXMASX"
-            ];
+async function main() {
+    const input = await fetch("https://adventofcode.com/2024/day/4/input", {
+        method: 'GET',
+        headers: {
+            'Cookie': process.env.COOKIE
+        }
+        }).then(response => response.text()).then(text => {
+            return text.split("\n");
+        });
+    var count = 0;
+    var count1 = 0;
     for (var i = 0; i < input.length; i++) {
         for (var j = 0; j < input[i].length; j++) {
-            console.log(input[i][j], i, j);
-            i,j i,j+1 i,j+2 i,j+3
-            //look horizontal
-            i,j i-1,j i-2,j i-3,j
-            //look vertical
-            i,j i-1,j+1 i-2,j+2 i-3,j+3
-            //look diagonal
-            //make everything backwards too
+            var needle = ["XMAS", "SAMX"];
+            try {
+                if (needle.includes(input[i][j]+input[i][j+1]+input[i][j+2]+input[i][j+3])) {
+                    count++;
+                }
+                if (needle.includes(input[i][j]+input[i-1][j]+input[i-2][j]+input[i-3][j])) {
+                    count++;
+                }
+                if (needle.includes(input[i][j]+input[i-1][j+1]+input[i-2][j+2]+input[i-3][j+3])) {
+                    count++;
+                }
+                if (needle.includes(input[i][j]+input[i-1][j-1]+input[i-2][j-2]+input[i-3][j-3])) {
+                    count++;
+                }
+            } catch {}
+            try {
+                if ( ( input[i-1][j-1]+input[i][j]+input[i+1][j+1] == "MAS" || input[i-1][j-1]+input[i][j]+input[i+1][j+1] == "SAM" ) && ( input[i-1][j+1]+input[i][j]+input[i+1][j-1] == "SAM" || input[i-1][j+1]+input[i][j]+input[i+1][j-1] == "MAS" ) ) {
+                    count1++;
+                }
+            } catch {}
         }
     }
+    console.log("Count: ", count);
+    console.log("Count 2: ", count1);
 }
